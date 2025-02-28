823b2dfd3a432d7eff258f3b72c829fbbd269e76
gaylesha <petr.vasilyev.1999@gmail.com>
2025-02-28 12:47:51 +0500

    made a function

--- src/Convertors/s21_from_decimal_to_int.c
+++ src/Convertors/s21_from_decimal_to_int.c
@@ -1,5 +1,26 @@
 #include "s21_convertors.h"
 
 int s21_from_decimal_to_int(s21_decimal src, int *dst) {  // В int
-  return 0;
+  if (!dst) {
+      return CONVERSION_ERROR;
+  }
+  
+  if (src.bits[1] != 0 || src.bits[2] != 0) {
+      return CONVERSION_ERROR;
+  }
+  
+  int scale = (src.bits[3] >> 16) & 0xFF;
+  
+  int value = src.bits[0];
+  
+  for (int i = 0; i < scale; i++) {
+      value /= 10;
+  }
+  
+  if (src.bits[3] & 0x80000000) {
+      value = -value;
+  }
+  
+  *dst = value;
+  return CNV_OK;
 }
