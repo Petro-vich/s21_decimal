8e262bb2491b483e2507eb1a794dbca7e0703edb
gaylesha <petr.vasilyev.1999@gmail.com>
2025-03-04 14:41:55 +0500

    Добавил две вспомогательные функции s21_bit_ckeck, s21_bit_add

--- src/Helpers/helpers.h
+++ src/Helpers/helpers.h
@@ -7,7 +7,7 @@ int s21_get_bit(s21_decimal decimal, int bit_position);
 int s21_set_bit(s21_decimal *decimal, int bit_position, int bit);
 int s21_get_exp(s21_decimal decimal); 
 int s21_get_sign(s21_decimal decimal);
-
-
+int s21_bit_check(unsigned int num, unsigned int bit_num);
+int s21_bit_add(unsigned num, unsigned bit_num);
 
 #endif
