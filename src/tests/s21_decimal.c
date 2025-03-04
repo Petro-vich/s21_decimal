3cefb3834961884c91467de0406d41ce545bcd2b
jacquela <jacquela@student.21-school.ru>
2025-03-04 15:58:25 +0500

    testsforDIV

--- src/tests/s21_decimal.c
+++ src/tests/s21_decimal.c
@@ -295,12 +295,211 @@ START_TEST(s21_from_decimal_to_int_6) {
 }
 END_TEST
 
+START_TEST(test_div_fail_manual1) {
+  // 792281625.14264337593543950335 / 792281625.14264337593543950335 = 1
+  s21_decimal decimal1 = {{0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0x140000}};
+  s21_decimal decimal2 = {{0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0x140000}};
+  s21_decimal result;
+  int code = s21_div(decimal1, decimal2, &result);
+
+  ck_assert_int_eq(code, TEST_ARITHMETIC_OK);
+  ck_assert_int_eq(result.bits[0], 1); // Результат должен быть 1
+  ck_assert_int_eq(result.bits[1], 0);
+  ck_assert_int_eq(result.bits[2], 0);
+  ck_assert_int_eq(result.bits[3], 0); // Масштаб и знак должны быть 0
+}
+END_TEST
+
+START_TEST(test_div_fail_manual2) {
+  // Деление на ноль
+  s21_decimal decimal1 = {{10, 0, 0, 0}};
+  s21_decimal decimal2 = {{0, 0, 0, 0}};
+  s21_decimal result;
+  int code = s21_div(decimal1, decimal2, &result);
+
+  ck_assert_int_eq(code, 3); // Ожидаем ошибку деления на ноль
+}
+END_TEST
+
+START_TEST(test_div_fail_manual3) {
+  // Деление нуля на число
+  s21_decimal decimal1 = {{0, 0, 0, 0}};
+  s21_decimal decimal2 = {{5, 0, 0, 0}};
+  s21_decimal result;
+  int code = s21_div(decimal1, decimal2, &result);
+
+  ck_assert_int_eq(code, TEST_ARITHMETIC_OK);
+  ck_assert_int_eq(result.bits[0], 0); // Результат должен быть 0
+  ck_assert_int_eq(result.bits[1], 0);
+  ck_assert_int_eq(result.bits[2], 0);
+  ck_assert_int_eq(result.bits[3], 0); // Масштаб и знак должны быть 0
+}
+END_TEST
+
+START_TEST(test_div_fail_manual4) {
+  // Деление отрицательного числа на положительное
+  s21_decimal decimal1 = {{10, 0, 0, 0x80000000}}; // -10
+  s21_decimal decimal2 = {{2, 0, 0, 0}}; // 2
+  s21_decimal result;
+  int code = s21_div(decimal1, decimal2, &result);
+
+  ck_assert_int_eq(code, TEST_ARITHMETIC_OK);
+  ck_assert_int_eq(result.bits[0], 5); // Результат должен быть -5
+  ck_assert_int_eq(result.bits[1], 0);
+  ck_assert_int_eq(result.bits[2], 0);
+  ck_assert_int_eq(result.bits[3], 0x80000000); // Знак отрицательный
+}
+END_TEST
+
+START_TEST(test_div_fail_manual5) {
+  // Деление с масштабированием
+  s21_decimal decimal1 = {{100, 0, 0, 0x10000}}; // 1.00
+  s21_decimal decimal2 = {{20, 0, 0, 0x10000}}; // 0.20
+  s21_decimal result;
+  int code = s21_div(decimal1, decimal2, &result);
+
+  ck_assert_int_eq(code, TEST_ARITHMETIC_OK);
+  ck_assert_int_eq(result.bits[0], 5); // Результат должен быть 5.00
+  ck_assert_int_eq(result.bits[1], 0);
+  ck_assert_int_eq(result.bits[2], 0);
+  ck_assert_int_eq(result.bits[3], 0x20000); // Масштаб 2
+}
+END_TEST
+
+START_TEST(test_div_fail_manual6) {
+  // Деление с переполнением
+  s21_decimal decimal1 = {{0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0}}; // Максимальное число
+  s21_decimal decimal2 = {{1, 0, 0, 0x1C0000}}; // 0.0000000000000000000000000001
+  s21_decimal result;
+  int code = s21_div(decimal1, decimal2, &result);
+
+  ck_assert_int_eq(code, 1); // Ожидаем ошибку переполнения
+}
+END_TEST
+
+START_TEST(test_div_fail_manual7) {
+  // Деление с отрицательным масштабом (некорректный масштаб)
+  s21_decimal decimal1 = {{10, 0, 0, 0x80010000}}; // -10 с масштабом 1
+  s21_decimal decimal2 = {{2, 0, 0, 0x80000000}}; // -2
+  s21_decimal result;
+  int code = s21_div(decimal1, decimal2, &result);
+
+  ck_assert_int_ne(code, TEST_ARITHMETIC_OK); // Ожидаем ошибку
+}
+END_TEST
+
+START_TEST(test_div_fail_manual8) {
+  // Деление с некорректным масштабом (масштаб > 28)
+  s21_decimal decimal1 = {{10, 0, 0, 0x1D0000}}; // Масштаб 29
+  s21_decimal decimal2 = {{2, 0, 0, 0}};
+  s21_decimal result;
+  int code = s21_div(decimal1, decimal2, &result);
+
+  ck_assert_int_ne(code, TEST_ARITHMETIC_OK); // Ожидаем ошибку
+}
+END_TEST
+
+START_TEST(test_div_fail_manual9) {
+  // Деление с некорректным масштабом (масштаб < 0)
+  s21_decimal decimal1 = {{10, 0, 0, 0xFFFFFFFF}}; // Некорректный масштаб
+  s21_decimal decimal2 = {{2, 0, 0, 0}};
+  s21_decimal result;
+  int code = s21_div(decimal1, decimal2, &result);
+
+  ck_assert_int_ne(code, TEST_ARITHMETIC_OK); // Ожидаем ошибку
+}
+END_TEST
+
+START_TEST(test_div_fail_manual10) {
+  // Деление с некорректными битами в масштабе
+  s21_decimal decimal1 = {{10, 0, 0, 0x1C0001}}; // Некорректные биты в масштабе
+  s21_decimal decimal2 = {{2, 0, 0, 0}};
+  s21_decimal result;
+  int code = s21_div(decimal1, decimal2, &result);
+
+  ck_assert_int_ne(code, TEST_ARITHMETIC_OK); // Ожидаем ошибку
+}
+END_TEST
+
+START_TEST(test_div_fail_manual11) {
+  // Деление с некорректными битами в масштабе (старшие биты)
+  s21_decimal decimal1 = {{10, 0, 0, 0x401C0000}}; // Некорректные биты в масштабе
+  s21_decimal decimal2 = {{2, 0, 0, 0}};
+  s21_decimal result;
+  int code = s21_div(decimal1, decimal2, &result);
+
+  ck_assert_int_ne(code, TEST_ARITHMETIC_OK); // Ожидаем ошибку
+}
+END_TEST
+
+START_TEST(test_div_fail_manual12) {
+  // Деление с некорректными битами в масштабе (младшие биты)
+  s21_decimal decimal1 = {{10, 0, 0, 0x11C0000}}; // Некорректные биты в масштабе
+  s21_decimal decimal2 = {{2, 0, 0, 0}};
+  s21_decimal result;
+  int code = s21_div(decimal1, decimal2, &result);
+
+  ck_assert_int_ne(code, TEST_ARITHMETIC_OK); // Ожидаем ошибку
+}
+END_TEST
+
+START_TEST(test_div_fail_manual13) {
+  // Деление с некорректными битами в масштабе (все биты установлены)
+  s21_decimal decimal1 = {{10, 0, 0, 0xFFFFFFFF}}; // Некорректные биты в масштабе
+  s21_decimal decimal2 = {{2, 0, 0, 0}};
+  s21_decimal result;
+  int code = s21_div(decimal1, decimal2, &result);
+
+  ck_assert_int_ne(code, TEST_ARITHMETIC_OK); // Ожидаем ошибку
+}
+END_TEST
+
+START_TEST(test_div_fail_manual14) {
+  // Деление с некорректными битами в масштабе (масштаб 28, но биты 0-15 не нули)
+  s21_decimal decimal1 = {{10, 0, 0, 0x1C0001}}; // Некорректные биты в масштабе
+  s21_decimal decimal2 = {{2, 0, 0, 0}};
+  s21_decimal result;
+  int code = s21_div(decimal1, decimal2, &result);
+
+  ck_assert_int_ne(code, TEST_ARITHMETIC_OK); // Ожидаем ошибку
+}
+END_TEST
+
+START_TEST(test_div_fail_manual15) {
+  // Деление с некорректными битами в масштабе (масштаб 28, но биты 24-30 не нули)
+  s21_decimal decimal1 = {{10, 0, 0, 0x401C0000}}; // Некорректные биты в масштабе
+  s21_decimal decimal2 = {{2, 0, 0, 0}};
+  s21_decimal result;
+  int code = s21_div(decimal1, decimal2, &result);
+
+  ck_assert_int_ne(code, TEST_ARITHMETIC_OK); // Ожидаем ошибку
+}
+END_TEST
+
+
+
 Suite *decimal_suite(void) {
   Suite *s;
   TCase *tc_core;
 
   s = suite_create("s21_decimal");
   tc_core = tcase_create("Core");
+  tcase_add_test(tc_core, test_div_fail_manual1);
+  tcase_add_test(tc_core, test_div_fail_manual2);
+  tcase_add_test(tc_core, test_div_fail_manual3);
+  tcase_add_test(tc_core, test_div_fail_manual4);
+  tcase_add_test(tc_core, test_div_fail_manual5);
+  tcase_add_test(tc_core, test_div_fail_manual6);
+  tcase_add_test(tc_core, test_div_fail_manual7);
+  tcase_add_test(tc_core, test_div_fail_manual8);
+  tcase_add_test(tc_core, test_div_fail_manual9);
+  tcase_add_test(tc_core, test_div_fail_manual10);
+  tcase_add_test(tc_core, test_div_fail_manual11);
+  tcase_add_test(tc_core, test_div_fail_manual12);
+  tcase_add_test(tc_core, test_div_fail_manual13);
+  tcase_add_test(tc_core, test_div_fail_manual14);
+  tcase_add_test(tc_core, test_div_fail_manual15);
+
 
   tcase_add_test(tc_core, test_fail);
   tcase_add_test(tc_core, test_decimal_ok);
