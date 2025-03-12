936a05643a9f007f94e4f98a1f7b1a860a33abc2
Petro-vich <petr.vasilyev.1999@gmail.com>
2025-03-12 17:59:54 +0500

    fix "retrun res"

--- src/Arithmetic/s21_add.c
+++ src/Arithmetic/s21_add.c
@@ -1,31 +1,43 @@
 #include "s21_arithmetic.h"
 
-int s21_add(s21_decimal value_1, s21_decimal value_2,
-            s21_decimal *result) {  // Addition	+
-  int res = 0, mem = 0;
-  for (int i = 0; i < 3; i++) {
-    for (int j = 0; j < 32; j++) {
-      switch ((s21_bit_check(value_1.bits[i], j) != 0) +
-              (s21_bit_check(value_2.bits[i], j) != 0) + (mem > 0)) {
-        case 1:
-          result->bits[i] = s21_bit_add(result->bits[i], j);
-          mem = 0;
-          break;
-        case 2:
-          mem = 1 << (j + 1);
-          if (mem < 0) mem = 1;
-          break;
-        case 3:
-          result->bits[i] = s21_bit_add(result->bits[i], j);
-          mem = 1 << (j + 1);
-          if (mem < 0) mem = 1;
-          break;
-        default:
-          break;
-      }
+int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
+  int sign1 = s21_get_sign(value_1);
+  int sign2 = s21_get_sign(value_2);
+  if (sign1 != sign2) {
+    if (sign1) {
+      return s21_sub(value_2, s21_absoulute_decimal(value_1), result);
+    } else {
+      return s21_sub(value_1, s21_absoulute_decimal(value_2), result);
     }
   }
-  if (mem) res = 1;
 
-  return res;
+  for (int i = 0; i < 4; i++) {
+    result->bits[i] = 0;
+  }
+
+  int carry = 0;
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 32; j++) {
+      int bit1 = s21_bit_check(value_1.bits[i], j) ? 1 : 0;
+      int bit2 = s21_bit_check(value_2.bits[i], j) ? 1 : 0;
+      int sum = bit1 + bit2 + carry;
+      if (sum % 2 == 1) {
+        result->bits[i] = s21_bit_add(result->bits[i], j);
+      }
+      carry = sum / 2;
+    }
+  }
+
+  int res_code = 0;
+  if (carry) {
+    res_code = sign1 ? 2 : 1;
+  }
+
+  if (sign1) {
+    result->bits[3] |= (1u << 31);
+  } else {
+    result->bits[3] &= ~(1u << 31);
+  }
+
+  return res_code;
 }
