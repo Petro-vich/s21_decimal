8973ed0862d7abc6d78f732d1b833c1eed884b98
Gayle Shaunte <gaylesha@ar-m3.chel.21-school.ru>
2025-03-25 16:46:50 +0500

    add tests for decimla to int

--- src/Convertors/s21_from_decimal_to_int.c
+++ src/Convertors/s21_from_decimal_to_int.c
@@ -1,26 +1,31 @@
 #include "s21_convertors.h"
 
-int s21_from_decimal_to_int(s21_decimal src, int *dst) {  // В int
-  if (!dst) {
-    return CONVERSION_ERROR;
-  }
+int s21_from_decimal_to_int(s21_decimal src, int *dst) {
+  if (!dst) return CONVERSION_ERROR;
 
-  if (src.bits[1] != 0 || src.bits[2] != 0) {
-    return CONVERSION_ERROR;
-  }
+  
+  if (src.bits[2] != 0 || src.bits[1] > 0x7FFFFFFF) return CONVERSION_ERROR;
 
   int scale = (src.bits[3] >> 16) & 0xFF;
 
-  int value = src.bits[0];
+  uint64_t value = ((uint64_t)src.bits[1] << 32) | src.bits[0];
 
+  
   for (int i = 0; i < scale; i++) {
     value /= 10;
   }
-
+  
+  if (value > 2147483648) return CONVERSION_ERROR;
+  
   if (src.bits[3] & 0x80000000) {
-    value = -value;
+    if (value == 2147483648) {  
+      *dst = -2147483648;
+    } else {
+      *dst = -(int)value;
+    }
+  } else {
+    *dst = (int)value;
   }
 
-  *dst = value;
   return CNV_OK;
 }
